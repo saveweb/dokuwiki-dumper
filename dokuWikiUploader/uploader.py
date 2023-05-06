c0e9b57582812d5e586da6d4d688db2743044339
yzqzss <yzqzss@yandex.com>
2023-05-06 14:00:57 +0800

    typo

--- dokuWikiUploader/uploader.py
+++ dokuWikiUploader/uploader.py
@@ -123,7 +123,7 @@ Dumped with DokuWiki-Dumper v{config.get('dokuWikiDumper_version')}, and uploade
         "subject": "; ".join(
             keywords_init
         ),  # Keywords should be separated by ; but it doesn't matter much; the alternative is to set one per field with subject[0], subject[1], ...
-        "uplode-state": "uploading",
+        "upload-state": "uploading",
     }
     language = info.get(INFO_LANG) if info.get(INFO_LANG) else config.get('lang')
     if language:
@@ -200,20 +200,20 @@ Dumped with DokuWiki-Dumper v{config.get('dokuWikiDumper_version')}, and uploade
         if (info.get(INFO_DOKU_URL) not in item.metadata.get("description") or
             'https://github.com/saveweb/dokuwiki-dumper' not in item.metadata.get("description")):
             # IA will format the description's HTML, so we can't just use `==` to compare.
-            print("    (add URL back)...")
+            print("    (add URL back: %s )..." % info.get(INFO_DOKU_URL))
             new_md.update({"description": description_with_URL})
         if item.metadata.get("last-updated-date") != time.strftime("%Y-%m-%d"):
-            print("    (update last-updated-date)...")
+            print("    (update last-updated-date to %s )..." % time.strftime("%Y-%m-%d"))
             new_md.update({"last-updated-date": time.strftime("%Y-%m-%d")})
         if item.metadata.get("subject") != "; ".join(keywords_full):
-            print("    (update subject)...")
+            print("    (update subject: %s )..." % "; ".join(keywords_full))
             new_md.update({"subject": "; ".join(keywords_full)})
         if item.metadata.get("originalurl") != info.get(INFO_DOKU_URL):
-            print("    (update originalurl)...")
+            print("    (update originalurl: %s )..." % info.get(INFO_DOKU_URL))
             new_md.update({"originalurl": info.get(INFO_DOKU_URL)})
-        if item.metadata.get("uplode-state") != "uploaded":
-            print("    (update uplode-state)...")
-            new_md.update({"uplode-state": "uploaded"})
+        if item.metadata.get("upload-state") != "uploaded":
+            print("    (update upload-state: uploaded)...")
+            new_md.update({"upload-state": "uploaded"})
 
         if new_md:
             r = item.modify_metadata(metadata=new_md,  # update
