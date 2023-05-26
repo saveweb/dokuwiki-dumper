e08ab40ae70d0d40d9c430ef77e5aff1d64e23d8
yzqzss <yzqzss@yandex.com>
2023-05-27 01:57:10 +0800

    refactor: Delay class with asyncio

--- dokuWikiDumper/utils/delay.py
+++ dokuWikiDumper/utils/delay.py
@@ -1,40 +1,59 @@
 import threading
-import time
+import asyncio
 
+
+delaying_theads = 0
+delaying_theads_lock = threading.Lock()
 
 class Delay:
     done: bool = False
     lock: threading.Lock = threading.Lock()
+    delay: float
 
-    def animate(self):
+    async def animate(self):
         while True:
             with self.lock:
                 if self.done:
                     return
 
-                print("\r" + self.ellipses, end="")
+                with delaying_theads_lock:
+                    print("\r" + self.ellipses, end="") if delaying_theads == 1 else None
                 self.ellipses += "."
 
-            time.sleep(0.3)
+            await asyncio.sleep(0.35)
 
-    def __init__(self, msg=None, delay=None):
+
+    def __init__(self, msg=None, delay: float=0.0):
         """Add a delay if configured for that"""
-        self.ellipses: str = "."
-
         if delay <= 0:
             return
+        self.ellipses: str = "."
+        self.delay = delay
 
+
+        with delaying_theads_lock:
+            global delaying_theads
+            delaying_theads += 1
+            
         if msg:
             self.ellipses = ("Delay %.1fs: %s " % (delay, msg)) + self.ellipses
         else:
             self.ellipses = ("Delay %.1fs " % (delay)) + self.ellipses
 
-        ellipses_animation = threading.Thread(target=self.animate)
-        ellipses_animation.daemon = True
-        ellipses_animation.start()
+        asyncio.run(self.async_tasks())
 
-        time.sleep(delay)
+    async def async_tasks(self):
+        task1 = asyncio.create_task(self.animate())
+        task2 = asyncio.create_task(self.task_done())
+        await task2
+        task1.cancel()
 
-        with self.lock:
+    async def task_done(self):
+        await asyncio.sleep(self.delay)
+
+        with self.lock and delaying_theads_lock:
             self.done = True
-            print("\r" + " " * len(self.ellipses) + "\r", end="")
+            global delaying_theads
+            delaying_theads -= 1
+            if delaying_theads == 0: # If this is the last thread, clear the line
+                print("\r" + " " * len(self.ellipses) + "\r", end="")
