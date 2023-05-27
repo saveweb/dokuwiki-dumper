ad69370dc940d0d69c0ba8dc12475ba8625ac960
yzqzss <yzqzss@yandex.com>
2023-05-27 18:27:28 +0800

    DumpLock: add windows compatibility

--- dokuWikiDumper/utils/dump_lock.py
+++ dokuWikiDumper/utils/dump_lock.py
@@ -1,10 +1,39 @@
-import fcntl
 import os
 import sys
+import importlib.util
+
 
 LOCK_FILENAME = 'dumping.lock'
 
-class DumpLock:
+class DumpLock_Basic:
+    def __init__(self, lock_dir):
+        self.lock_file = os.path.join(lock_dir, LOCK_FILENAME)
+
+    def __enter__(self):
+        if os.path.exists(self.lock_file):
+            with open(self.lock_file, 'r', encoding='utf-8') as f:
+                print(f.read())
+            sys.exit("Another instance is already running, quitting...")
+        else:
+            with open(self.lock_file, 'w', encoding='utf-8') as f:
+                f.write(f'PID: {os.getgid()}: Running')
+            print("Acquired lock, continuing.")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.remove(self.lock_file)
+        print("Released lock.")
+
+    # decorator
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            with self:
+                return func(*args, **kwargs)
+        return wrapper
+
+
+class DumpLock_Fcntl():
+    import fcntl
+
     def __init__(self, lock_dir):
         self.lock_file = os.path.join(lock_dir, LOCK_FILENAME)
         self.lock_file_fd = None
@@ -12,7 +41,7 @@ class DumpLock:
     def __enter__(self):
         self.lock_file_fd = open(self.lock_file, 'w')
         try:
-            fcntl.lockf(self.lock_file_fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            self.fcntl.lockf(self.lock_file_fd, self.fcntl.LOCK_EX | self.fcntl.LOCK_NB)
             print("Acquired lock, continuing.")
         except IOError:
             print("Another instance is already running, quitting.")
@@ -21,7 +50,7 @@ class DumpLock:
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.lock_file_fd is None:
             raise IOError("Lock file not opened.")
-        fcntl.lockf(self.lock_file_fd, fcntl.LOCK_UN)
+        self.fcntl.lockf(self.lock_file_fd, self.fcntl.LOCK_UN)
         self.lock_file_fd.close()
         os.remove(self.lock_file)
         print("Released lock.")
@@ -32,3 +61,11 @@ class DumpLock:
             with self:
                 return func(*args, **kwargs)
         return wrapper
+
+class DumpLock():
+    def __new__(cls, lock_dir):
+        fcntl_avaivable = importlib.util.find_spec('fcntl')
+        if fcntl_avaivable is not None:
+            return DumpLock_Fcntl(lock_dir)
+        else:
+            return DumpLock_Basic(lock_dir)
